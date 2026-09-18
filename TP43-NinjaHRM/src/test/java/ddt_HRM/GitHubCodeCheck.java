a108340e8d4f6427e28f227a9eb5903eaf4e2973
Administrator <Administrator@DESKTOP-NEII4G1.mshome.net>
2026-09-18 13:06:40 +0530

    Adding new class GitHubCodeCheck

---
+++ TP43-NinjaHRM/src/test/java/ddt_HRM/GitHubCodeCheck.java
@@ -0,0 +1,18 @@
+package ddt_HRM;
+
+public class GitHubCodeCheck {
+	
+	public static void add()
+	{
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+		
+	}
+
+	public static void main(String[] args) 
+	{
+		add();
+	}
+
+}
